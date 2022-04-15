87e27e12c2c8db58dca13317f7a6ee6a79806442
AnimaDigitalisStudio <nicola.gernone@animadigitalis.com>
2022-04-15 11:31:13 +0200

    setting unity

--- Assets/Scripts/machu.cs
+++ Assets/Scripts/machu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Start : MonoBehaviour
+public class Machu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
